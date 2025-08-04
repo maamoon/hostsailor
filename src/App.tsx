5b0bb42b081fb1b7a04f74c0ccfdb11f259c64e9
maamoon <maamoon_momani@hotmail.com>
2025-08-04 22:55:56 +0300

    Fix React Router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/hostsailor">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
